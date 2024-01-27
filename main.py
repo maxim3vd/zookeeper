aae1d62f35ff7a70ec9976f48de19237d641b8f7
maxim3andr3 <maxim3.vd@gmail.com>
2024-01-27 10:15:44 +0100

    Display end-of-program message and terminate the program if the user enter the word "exit"

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ exit_program = False
 
 while not exit_program:
     user_input = input("Please enter the number of the habitat you would like to view: ")
-    habitat_number = int(user_input)
 
-    if 0 <= habitat_number < len(animals):
-        print(animals[habitat_number])
+    if user_input.lower() == "exit":
+        print("See you later!")
+        exit_program = True
+    else:
+        habitat_number = int(user_input)
+
+        if 0 <= habitat_number < len(animals):
+            print(animals[habitat_number])
