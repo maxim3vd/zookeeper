64b7cd3d134a57d6cf13fbde6b568bacc1b09f75
maxim3andr3 <maxim3.vd@gmail.com>
2024-01-27 10:09:59 +0100

    Validate habitat number and display corresponding animal ASCII art

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+from animals import animals
+
 user_input = input("Please enter the number of the habitat you would like to view: ")
 habitat_number = int(user_input)
+
+if 0 <= habitat_number < len(animals):
+    print(animals[habitat_number])
