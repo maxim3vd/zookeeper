ae8dc28d7fcf5109735de5e8e9babd417d9562d1
maxim3andr3 <maxim3.vd@gmail.com>
2024-01-27 10:10:22 +0100

    Display end-of-program message

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ habitat_number = int(user_input)
 
 if 0 <= habitat_number < len(animals):
     print(animals[habitat_number])
+
+print("---\nYou've reached the end of the program. To check another habitat, please restart the watcher.")
